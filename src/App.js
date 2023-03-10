d772a6999a4e5ee647dd66efabece5e3a022118e
Abhishek <abhishekkhangare21@gmail.com>
2023-03-10 19:37:30 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index.js";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App Using Redux Toolkit</h1>
+      <h1>{counter}</h1>
+      <div>
+        <button className="btn_blue" onClick={increment}>
+          Increment
+        </button>
+        <button className="btn_red" onClick={decrement}>
+          Decrement
+        </button>
+      </div>
+      <button className="btn_green" onClick={addBy}>
+        Add Value 10
+      </button>
     </div>
   );
 }
